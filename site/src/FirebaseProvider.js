4c6919ac3a3dc61de8b8848bbbb03b69b35dec89
Aquib Master <aquibmaster@gmail.com>
2018-03-27 17:29:05 +1300

    First pass at surfacing stats data

---
+++ site/src/FirebaseProvider.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default (Component, firebaseApp, ref) =>
+    class extends React.Component {
+        state = {
+            data: {},
+        }
+
+        firebaseRef = firebaseApp.database().ref(ref)
+
+        componentDidMount() {
+            this.firebaseRef.on('value', this._handleNewFirebaseData)
+        }
+
+        componentWillUnmount() {
+            this.firebaseRef.off('value', this._handleNewFirebaseData)
+        }
+
+        _handleNewFirebaseData = snapshot => {
+            this.setState({ data: snapshot.val() })
+        }
+
+        render() {
+            return <Component {...this.props} data={this.state.data} />
+        }
+    }
--- site/src/index.js
+++ site/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import firebase from 'firebase'
+
+import FirebaseProvider from './FirebaseProvider'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.css'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: 'AIzaSyCLaXg1NbSEPoHNP1YXvYITTQF6EmwWe8Y',
+    authDomain: 'everyonebotdev.firebaseapp.com',
+    databaseURL: 'https://everyonebotdev.firebaseio.com',
+    projectId: 'everyonebotdev',
+})
+
+const ConnectedApp = FirebaseProvider(App, firebaseApp, '/statistics')
+
+ReactDOM.render(<ConnectedApp />, document.getElementById('root'))
