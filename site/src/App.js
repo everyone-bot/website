9ab80fc89c7cbe651416d796067a3a7613372c8d
Aquib Master <aquibmaster@gmail.com>
2018-04-16 16:53:07 +1200

    First pass at stats display

--- site/src/App.js
+++ site/src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
-import Logo from './logo.png'
 
 import './app.css'
 
 class App extends Component {
+    numbers = '1234567890,.'
+
+    colors = [
+        'aqua',
+        'cadetblue',
+        'coral',
+        'crimson',
+        'azure',
+        'darkviolet',
+        'rebeccapurple',
+        'gold',
+        'palegreen',
+    ]
+
     state = {
         isReady: false,
+        displayStatistics: {
+            mentions: '',
+            users: '',
+            groups: '',
+            largestGroup: '',
+        },
+        statisticAnimationFrame: {
+            mentions: -1,
+            users: -1,
+            groups: -1,
+            largestGroup: -1,
+        },
     }
 
     componentWillReceiveProps({ data }) {
@@ -13,67 +38,121 @@ class App extends Component {
         if (!oldData && data) this.setState({ isReady: true })
     }
 
-    renderBody() {
+    computeStatistic(sampleWidth, statistic) {
+        const { isReady, statisticAnimationFrame } = this.state
         const { data } = this.props
 
+        if (!isReady) {
+            const string = this.generateString(sampleWidth)
+
+            setTimeout(() => this.computeStatistic(sampleWidth, statistic), 100)
+
+            this.setState(state => ({
+                displayStatistics: {
+                    ...state.displayStatistics,
+                    [statistic]: string,
+                },
+            }))
+
+            return
+        }
+
+        const actualStatistic = data[statistic].toLocaleString()
+        const randomString = this.generateString(actualStatistic.length)
+        const currentFrame = statisticAnimationFrame[statistic]
+        const displayValue = randomString
+            .split('')
+            .map(
+                (item, idx) =>
+                    idx < currentFrame ? actualStatistic.charAt(idx) : item,
+            )
+            .join('')
+
+        if (currentFrame < actualStatistic.length) {
+            setTimeout(() => this.computeStatistic(sampleWidth, statistic), 100)
+        }
+
+        this.setState(state => ({
+            displayStatistics: {
+                ...state.displayStatistics,
+                [statistic]: displayValue,
+            },
+            statisticAnimationFrame: {
+                ...state.statisticAnimationFrame,
+                [statistic]: currentFrame + 1,
+            },
+        }))
+    }
+
+    renderStatistic(statistic) {
+        const { displayStatistics, statisticAnimationFrame } = this.state
+        if (!displayStatistics[statistic]) return null
+
+        const renderValue = displayStatistics[statistic].split('')
+        const currentFrame = statisticAnimationFrame[statistic]
+
         return (
-            <div className="app__body">
-                <h1 className="app__heading">Everyone Bot</h1>
+            <span>
+                {renderValue.map((letter, idx) => {
+                    const color =
+                        idx > currentFrame
+                            ? this.generateRandomColor()
+                            : 'white'
 
-                <a
-                    role="button"
-                    className="app__button"
-                    href="https://www.google.com"
-                >
-                    Add to your group
-                </a>
-
-                <div className="app__mentions">
-                    {data.mentions.toLocaleString()}
-                </div>
-
-                <div className="app__stats">
-                    <div className="app__stat">
-                        {data.groups.toLocaleString()} groups
-                    </div>
-
-                    <div className="app__stat">
-                        {data.users.toLocaleString()} users
-                    </div>
-
-                    <div className="app__stat">
-                        {data.largestGroup.toLocaleString()} largest group
-                    </div>
-                </div>
-            </div>
+                    return (
+                        <span key={idx} style={{ color }}>
+                            {letter}
+                        </span>
+                    )
+                })}
+            </span>
         )
     }
 
-    renderFooter() {
-        return (
-            <footer className="app__footer">
-                Made with love by Aquib Master
-            </footer>
-        )
+    generateString(length) {
+        const numArray = this.numbers.split('')
+        return new Array(length)
+            .fill(undefined)
+            .map(
+                item =>
+                    numArray[Math.floor(Math.random() * this.numbers.length)],
+            )
+            .join('')
+    }
+
+    generateRandomColor() {
+        return this.colors[Math.floor(Math.random() * this.colors.length)]
+    }
+
+    componentDidMount() {
+        this.computeStatistic(10, 'mentions')
+        this.computeStatistic(5, 'users')
+        this.computeStatistic(5, 'groups')
+        this.computeStatistic(3, 'largestGroup')
     }
 
     render() {
-        const { isReady } = this.state
-        const logoClasses = [
-            'app__logo',
-            !isReady && 'app__logo--loading',
-        ].join(' ')
-
         return (
             <div className="app">
-                <div className={logoClasses}>
-                    <img alt="Everyone bot logo" src={Logo} title="logo" />
-                </div>
+                <h1>@everyone_bot</h1>
 
-                {isReady && [this.renderBody(), this.renderFooter()]}
+                <p>
+                    Telegram bot that makes it easy to mention{' '}
+                    <span className="app__highlight">@everyone</span> in a group
+                </p>
+
+                <p>
+                    {this.renderStatistic('mentions')} mentions sent |{' '}
+                    {this.renderStatistic('users')} users |{' '}
+                    {this.renderStatistic('groups')} groups |{' '}
+                    {this.renderStatistic('largestGroup')} users in the largest
+                    group
+                </p>
             </div>
         )
     }
 }
 
 export default App
+
+// Telegram bot that makes it easy to mention everyone in a group
